954ac7a27cd6537c544b28fe12de7fc0ace238b6
Noureddine Latreche <nourwushu@gmail.com>
2021-05-27 09:35:19 +0100

    added auth.json #2

--- .platform/confighooks/predeploy/01_link.sh
+++ .platform/confighooks/predeploy/01_link.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 if [[ -f "app/etc/env.php" ]]; then rm -f app/etc/env.php; fi; if [ -f "/mnt/efs/app/etc/env.php" ]; then cp -f /mnt/efs/app/etc/env.php app/etc/env.php; fi;
+if [[ -f "auth.json" ]]; then rm -f auth.json; fi; if [ -f "/mnt/efs/auth.json" ]; then cp -f /mnt/efs/auth.json auth.json; fi;
